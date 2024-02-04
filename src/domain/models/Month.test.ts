d31e9ec27f543a3495c1dbf47c99d64f605b0af3
Daniel Ramos <danielramosacosta1@gmail.com>
2024-02-04 19:14:42 +0100

    fix: test daysBefore

--- src/domain/models/Month.test.ts
+++ src/domain/models/Month.test.ts
@@ -8,7 +8,7 @@ describe("Month", () => {
   describe("daysBefore", () => {
     it("filters all the days now is before the month", () => {
       const month = new Month(MonthOfTheYear.at(2021, 10), generate31Days());
-      const now = new Date("2021-09-01");
+      const now = new Date("2021-09-01T14:00:00.000Z");
 
       const daysBefore = month.daysBefore(now);
 
@@ -17,7 +17,7 @@ describe("Month", () => {
 
     it("returns all days if now is after the month", () => {
       const month = new Month(MonthOfTheYear.at(2021, 9), generate31Days());
-      const now = new Date("2021-10-01");
+      const now = new Date("2021-10-01T14:00:00.000Z");
 
       const daysBefore = month.daysBefore(now);
 
@@ -26,7 +26,7 @@ describe("Month", () => {
 
     it("returns all days that are after now", () => {
       const month = new Month(MonthOfTheYear.at(2021, 9), generate31Days());
-      const now = new Date("2021-09-05");
+      const now = new Date("2021-09-05T14:00:00.000Z");
 
       const daysBefore = month.daysBefore(now);
 
